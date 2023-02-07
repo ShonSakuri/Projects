d38284849e87cfd72aaf0520c13e371bba2df9b4
ShonDevelopment <106615918+ShonDevelopment@users.noreply.github.com>
2023-02-07 11:13:24 +0200

    change

--- Bank/bank.py
+++ Bank/bank.py
@@ -1,18 +1,15 @@
 import pymongo
 import sys
 import tty
-import time
 import os
 import termios
 from colorama import Fore, Style
+import getpass
 
-
-client = pymongo.MongoClient(
-    "MongoKey")
-
-db = client["Bank"]
-collection = db["users"]
-
+class Database:
+    client = pymongo.MongoClient("MongoDB KEY")
+    db = client["Bank"]
+    collection = db["users"]
 
 def getch():
     fd = sys.stdin.fileno()
@@ -24,21 +21,18 @@ def getch():
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
 
-
-char = getch()
-
-print("""
-Register - You need to click 1 Key to Register.
-Login - You need to click 2 Key to Login.
-Reset Password - You need to click 3 key to reset your password.
-""")
-
+def menu():
+    print(Fore.LIGHTMAGENTA_EX,"""
+    1. Register
+    2. Login
+    3. Reset Password
+    """)
+    print(Style.RESET_ALL)
 
 def admin():
     tr = True
     while tr:
-        commands = ["user", "removemoney", "addmoney",
-                    "quit", "setmoney", "deleteuser"]
+        commands = ["user","removemoney","addmoney","quit","setmoney","deleteuser"]
         print(Fore.YELLOW, commands)
         print(Style.RESET_ALL)
         command = input("command <-> ").lower()
@@ -48,72 +42,70 @@ def admin():
 
         elif command == "removemoney":
             usr = input("user <-> ")
-            user = collection.find_one({"username": usr})
+            user = Database.collection.find_one({"username": usr})
             if not user:
                 print(Fore.RED, "User not found.")
                 print(Style.RESET_ALL)
             if user:
-                user = collection.find_one({"username": usr})
+                user = Database.collection.find_one({"username": usr})
                 balance = user['bank']
                 removeMoney = int(input("Money <-> "))
                 money = balance - removeMoney
                 filter = {"username": usr}
                 newvalues = {"$set": {'bank': money}}
-                collection.update_one(filter, newvalues)
+                Database.collection.update_one(filter, newvalues)
 
         elif command == "addmoney":
             usr = input("user <-> ")
-            user = collection.find_one({"username": usr})
+            user = Database.collection.find_one({"username": usr})
             if not user:
                 print(Fore.RED, "User not found.")
                 print(Style.RESET_ALL)
             if user:
-                user = collection.find_one({"username": usr})
+                user = Database.collection.find_one({"username": usr})
                 balance = user['bank']
                 addMoney = int(input("Money <-> "))
                 money = addMoney + balance
                 filter = {"username": usr}
                 newvalues = {"$set": {'bank': money}}
-                collection.update_one(filter, newvalues)
+                Database.collection.update_one(filter, newvalues)
 
         elif command == "setmoney":
             usr = input("user <-> ")
-            user = collection.find_one({"username": usr})
+            user = Database.collection.find_one({"username": usr})
             if not user:
                 print(Fore.RED, "User not found.")
                 print(Style.RESET_ALL)
             if user:
-                user = collection.find_one({"username": usr})
+                user = Database.collection.find_one({"username": usr})
                 SetMoney = int(input("Money <-> "))
                 filter = {"username": usr}
                 newvalues = {"$set": {'bank': SetMoney}}
-                collection.update_one(filter, newvalues)
+                Database.collection.update_one(filter, newvalues)
 
         elif command == "deleteuser":
             usr = input("user <-> ")
-            user = collection.find_one({"username": usr})
+            user = Database.collection.find_one({"username": usr})
             if not user:
                 print(Fore.RED, "User not found.")
                 print(Style.RESET_ALL)
             if user:
-                collection.delete_one({"username": usr})
+                Database.collection.delete_one({"username": usr})
         else:
             print(Fore.RED, "The command not valid.")
             print(Style.RESET_ALL)
 
-
 def login():
     username = input("username <-> ")
-    password = input("password <-> ")
-    user = collection.find_one({"username": username, "password": password})
+    password = getpass.getpass("password <-> ")
+    user = Database.collection.find_one({"username": username, "password": password})
     os.system("clear")
 
     if not user:
         print(Fore.RED, "User not found")
         print(Style.RESET_ALL)
     else:
-        user = collection.find_one(
-            {"username": username, "password": password})
+        user = Database.collection.find_one({"username": username, "password": password})
         if user['admin'] == "True":
             admin()
             exit()
@@ -122,91 +114,66 @@ def login():
             print(Style.RESET_ALL)
 
             def user():
-                rr = True
-                while rr:
-                    user = collection.find_one(
-                        {"username": username, "password": password})
-                    commands = ["quit", "balance", "cash",
-                                "deposit", "withdraw", "currency", "sendmoney"]
+                while True:
+                    user = Database.collection.find_one({"username": username, "password": password})
+                    commands = ["quit","balance","cash","deposit","withdraw","currency","transfer"]
                     print(Fore.YELLOW, commands)
                     print(Style.RESET_ALL)
                     command = input("command <-> ").lower()
 
                     if command == "quit":
-                        rr = False
+                        return
 
                     elif command == "balance":
-                        user = collection.find_one(
+                        user = Database.collection.find_one(
                             {"username": username, "password": password})
                         print(user['bank'])
 
                     elif command == "cash":
-                        user = collection.find_one(
+                        user = Database.collection.find_one(
                             {"username": username, "password": password})
                         print(user['cash'])
 
                     elif command == "deposit":
-                        main = collection.find_one(
-                            {"username": username, "password": password})
+                        main = Database.collection.find_one({"username": username, "password": password})
                         if main:
                             balance = main['bank']
                             _cash = main['cash']
                             amount = int(input("Money <-> "))
                             if _cash < 0:
-                                print(
-                                    Fore.RED, f"You can't deposit that amount of money you have {_cash} in your wallet.")
+                                print(Fore.RED, f"You can't deposit that amount of money you have {_cash} in your wallet.")
                                 print(Style.RESET_ALL)
-                                amm = balance - amount
-                                print(amm)
-                        #     elif amm < :
-                                
                             else:
-                              filter = {"username": username,
-                                          "password": password}
-                              
-                              newvalues = {
-                                    "$set": {'cash': _cash - amount}
-                                    }
-                                    
-                              collection.update_one(filter, newvalues)
+                              filter = {"username": username,"password": password}
+                              newvalues = {"$set": {'cash': _cash - amount}}
+                              Database.collection.update_one(filter, newvalues)
                               money = amount + balance
                               filter = {"username": username}
                               newvalues = {"$set": {'bank': money}}
-                              collection.update_one(filter, newvalues)
+                              Database.collection.update_one(filter, newvalues)
                               print(Fore.GREEN, "successfully withdraw the money.")
                               print(Style.RESET_ALL)
 
                     elif command == "withdraw":
-                        main = collection.find_one(
-                            {"username": username, "password": password})
+                        main = Database.collection.find_one({"username": username, "password": password})
                         if main:
                             balance = main['bank']
                             _cash = main['cash']
                             amount = int(input("Money <-> "))
                             if balance < -5000:
-                                print(
-                                    Fore.RED, f"You can't withdraw that amount of money you have {balance} in your bank.")
+                                print(Fore.RED, f"You can't withdraw that amount of money you have {balance} in your bank.")
                                 print(Style.RESET_ALL)
-                                amm = balance - amount
-                                print(amm)
-                        #     elif amm < :
-                                
                             else:
-                                filter = {"username": username,
-                                          "password": password}
-                                newvalues = {
-                                    "$set": {'bank': balance - amount},
-                                    "$set": {'cash': _cash + amount}
-                                    }
-                                
-                                collection.update_one(filter, newvalues)
+                                filter = {"username": username,"password": password}
+                                newvalues = {"$set": {'bank': balance - amount},"$set": {'cash': _cash + amount}}
+                                Database.collection.update_one(filter, newvalues)
                                 print(Fore.GREEN, "successfully withdraw the money.")
                                 print(Style.RESET_ALL)
 
-                    elif command == "sendmoney":
+                    elif command == "transfer":
                         usr = input("user <-> ")
-                        user = collection.find_one({"username": usr})
-                        main = collection.find_one(
+                        user = Database.collection.find_one({"username": usr})
+                        main = Database.collection.find_one(
                             {"username": username, "password": password})
                         if not user:
                             print(Fore.RED, "User not found.")
@@ -223,21 +190,18 @@ def login():
                                 filter = {"username": usr}
                                 newvalues = {
                                     "$set": {'bank': balance2 + amount}}
-                                collection.update_one(filter, newvalues)
-                                filter = {"username": username,
-                                          "password": password}
-                                newvalues = {
-                                    "$set": {'bank': balance - amount}}
-                                collection.update_one(filter, newvalues)
+                                Database.collection.update_one(filter, newvalues)
+                                filter = {"username": username,"password": password}
+                                newvalues = {"$set": {'bank': balance - amount}}
+                                Database.collection.update_one(filter, newvalues)
                                 print(Fore.GREEN, "Money sent successfully.")
                                 print(Style.RESET_ALL)
             user()
 
-
 def register():
     username = input("username <-> ")
-    password = input("password <-> ")
-    user = collection.find_one({"username": username})
+    password = getpass.getpass("password <-> ")
+    user = Database.collection.find_one({"username": username})
     secret = input(
         "Enter your secret word.\ncan be your Dog name or maybe your nickname.\nYou have to remember it after you write it (Min of 2 characters).")
     if len(secret) < 2:
@@ -257,18 +221,16 @@ def register():
                   "The password has to be 8 or more length Try again.")
             print(Style.RESET_ALL)
             register()
-        collection.insert_one({'username': username, 'password': password,
-                              'bank': 0, 'cash': 5000, 'secret_word': secret, 'admin': "False"})
+        Database.collection.insert_one({'username': username, 'password': password,'bank': 0, 'cash': 5000, 'secret_word': secret, 'admin': "False"})
         print(Fore.LIGHTGREEN_EX, "User Registered.")
         print(Style.RESET_ALL)
-
 
 def reset():
     secret_word = input("Enter your account Secret Key.")
     usr = input("user <-> ")
-    usr2 = collection.find_one({"username": usr})
+    usr2 = Database.collection.find_one({"username": usr})
     if usr2['secret_word'] == secret_word:
-        password = input("Enter the new password.")
+        password = getpass.getpass('Enter the new password:')
         if len(password) < 8:
             os.system("clear")
             print(Fore.LIGHTRED_EX,
@@ -277,16 +239,19 @@ def reset():
             reset()
         filter = {"username": usr}
         newvalues = {"$set": {'password': password}}
-        collection.update_one(filter, newvalues)
+        Database.collection.update_one(filter, newvalues)
     else:
         print(Fore.LIGHTRED_EX, "The secret you mention not valid\nTry again.")
         print(Style.RESET_ALL)
         reset()
 
+if __name__ == "__main__":
+    menu()
+    char = getch()
 
-if char == "1":
-    register()
-if char == "2":
-    login()
-if char == "3":
-    reset()
+    if char == "1":
+        register()
+    if char == "2":
+        login()
+    if char == "3":
+        reset()
